c8cc240b61114a72e41f1e03b8a882596c975a89
Iqbal Maulana <iq.bluejack@gmail.com>
2022-09-11 15:09:14 +0700

    feat: add support for pandawa 5

--- src/PgBouncerBundle.php
+++ src/PgBouncerBundle.php
@@ -5,16 +5,22 @@ declare(strict_types=1);
 namespace Pandawa\PgBouncer;
 
 use Illuminate\Database\Connection;
-use Pandawa\Component\Module\AbstractModule;
+use Illuminate\Database\PostgresConnection;
+use Pandawa\Component\Foundation\Bundle\Bundle;
+use PDO;
 
 /**
  * @author  Iqbal Maulana <iq.bluejack@gmail.com>
  */
-class PandawaPgBouncerModule extends AbstractModule
+class PgBouncerBundle extends Bundle
 {
-    protected function init(): void
+    public function configure(): void
     {
         Connection::resolverFor('pgsql', function ($connection, $database, $prefix, $config) {
+            if (in_array(PDO::ATTR_EMULATE_PREPARES, $config['options'] ?? [])) {
+                return new PgBouncerConnection($connection, $database, $prefix, $config);
+            }
+
             return new PostgresConnection($connection, $database, $prefix, $config);
         });
     }
--- src/PgBouncerConnection.php
+++ src/PgBouncerConnection.php
@@ -5,33 +5,14 @@ declare(strict_types=1);
 namespace Pandawa\PgBouncer;
 
 use DateTimeInterface;
-use Illuminate\Database\PostgresConnection as BasePostgresConnection;
+use Illuminate\Database\PostgresConnection;
 use PDO;
 
 /**
  * @author  Iqbal Maulana <iq.bluejack@gmail.com>
  */
-class PostgresConnection extends BasePostgresConnection
+class PgBouncerConnection extends PostgresConnection
 {
-    public function bindValues($statement, $bindings)
-    {
-        foreach ($bindings as $key => $value) {
-            if (is_int($value)) {
-                $pdoParam = PDO::PARAM_INT;
-            } elseif (is_resource($value)) {
-                $pdoParam = PDO::PARAM_LOB;
-            } else {
-                $pdoParam = PDO::PARAM_STR;
-            }
-
-            $statement->bindValue(
-                is_string($key) ? $key : $key + 1,
-                $value,
-                $pdoParam
-            );
-        }
-    }
-
     public function prepareBindings(array $bindings)
     {
         $grammar = $this->getQueryGrammar();
